a556e7f5a1b0cba37a5f289161b9243b43981a2e
thapaRoyal <thaparoyal17@gmail.com>
2021-10-27 22:06:08 +0545

    details component

--- src/components/Details/Dteails.jsx
+++ src/components/Details/Dteails.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
 
 const Dteails = () => {
-  return <div></div>;
+  return (
+    <Card>
+      <cardHeader title="Income" />
+      <CardContent>
+        <Typography variant="body2">$50</Typography>
+        <Doughnut data="DATA" />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Dteails;
