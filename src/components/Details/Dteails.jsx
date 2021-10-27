2bbcc01a37d6a851ada9f0caf7bab5c67fad3ed9
thapaRoyal <thaparoyal17@gmail.com>
2021-10-27 22:03:25 +0545

    starting details component

---
+++ src/components/Details/Dteails.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+const Dteails = () => {
+  return <div></div>;
+};
+
+export default Dteails;
