165265783def9c9a2b821dbf401cd455cd045f76
thapaRoyal <thaparoyal17@gmail.com>
2021-10-29 14:00:08 +0545

    styles

--- src/components/main/Form/Form.jsx
+++ src/components/main/Form/Form.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import {
+  TextField,
+  Typography,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
 
 const Form = () => {
   return <div></div>;
