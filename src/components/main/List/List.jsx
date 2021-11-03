198bdf9c2d723321f6bdeb3328d2e760ad9ce9d2
thapaRoyal <thaparoyal17@gmail.com>
2021-11-03 18:34:50 +0545

    imports

--- src/components/main/List/List.jsx
+++ src/components/main/List/List.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {
+  List as MUIList,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Avatar,
+  Slide,
+} from '@material-ui/core';
+import { Delete, MOneyOff } from '@material-ui/icons';
+
+import useStyles from './styles';
 
 const List = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const classes = useStyles();
+  return <div></div>;
+};
 
-export default List
+export default List;
