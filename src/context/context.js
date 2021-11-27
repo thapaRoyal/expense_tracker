6c64e0a98da8de976359cf4d4777e553a1ef38a5
thapaRoyal <thaparoyal17@gmail.com>
2021-11-27 15:24:25 +0545

    transactions

--- src/context/context.js
+++ src/context/context.js
@@ -18,7 +18,12 @@ export const Provider = ({ children }) => {
     dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
   };
   return (
-    <ExpenseTrackerContext.Provider value={{ appName: 'Expense Tracker' }}>
+    <ExpenseTrackerContext.Provider
+      value={{
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </ExpenseTrackerContext.Provider>
   );
