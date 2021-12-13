4bbfb2442214071939da47a8ca4c88d5189aa078
thapaRoyal <thaparoyal17@gmail.com>
2021-12-13 14:44:19 +0545

    delete transactions

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,3 +1,14 @@
-const contextReducer = (state, action) => {};
+const contextReducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      const transactions = state.filter((t) => t.id !== action.payload);
+      return transactions;
+    case 'ADD_TRANSACTION':
+      break;
+
+    default:
+      break;
+  }
+};
 
 export default contextReducer;
